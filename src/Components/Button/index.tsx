4b42ac1cde08abfa385268a6287dd7fcebf1604d
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-01-29 10:47:46 -0700

    Got bombs displaying on open squares 27min05sec on Tutorial2

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { CellState, CellValue } from "../../types";
 import "./Button.scss";
 
 interface ButtonProps {
-
+    row: number;
+    col: number;
+    state: CellState;
+    value: CellValue;
 }
 
-const Button: React.FC<ButtonProps> = () => {
-    return <div className="Button" />;
+const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+    
+    const renderContent = (): React.ReactNode => { // <!--- renderContent()
+        if (state === CellState.visible) {
+            if (value === CellValue.bomb) {
+                return <span role='img' aria-label="bomb">💣</span>
+            }
+        } else if (state === CellState.flagged) {
+            return <span role='img' aria-label="bomb">🏴‍☠️</span>
+        }
+        return null
+    }; // <!------------------------------------------------ renderContent()
+    
+    return (
+        <div className={`Button ${state === CellState.visible ? "visible" : ""}`}>
+            {renderContent()}
+        </div>
+    );
 };
 
 export default Button;
