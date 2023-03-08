4f81832ddbb1bc2b7a10f4c10fde1a5ee03d96ba
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-03-08 13:18:38 -0700

    added .gitignore file and deleted node modules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,26 +15,14 @@ const grabAllAdjacentCells = (
   bottomCell: Cell | null;
   bottomRightCell: Cell | null;
 } => {
-  const topLeftCell =
-    rowParam > 0 && colParam > 0 ? cells[rowParam - 1][colParam - 1] : null;
+  const topLeftCell = rowParam > 0 && colParam > 0 ? cells[rowParam - 1][colParam - 1] : null;
   const topCell = rowParam > 0 ? cells[rowParam - 1][colParam] : null;
-  const topRightCell =
-    rowParam > 0 && colParam < MAX_COLS - 1
-      ? cells[rowParam - 1][colParam + 1]
-      : null;
+  const topRightCell = rowParam > 0 && colParam < MAX_COLS - 1 ? cells[rowParam - 1][colParam + 1] : null;
   const leftCell = colParam > 0 ? cells[rowParam][colParam - 1] : null;
-  const rightCell =
-    colParam < MAX_COLS - 1 ? cells[rowParam][colParam + 1] : null;
-  const bottomLeftCell =
-    rowParam < MAX_ROWS - 1 && colParam > 0
-      ? cells[rowParam + 1][colParam - 1]
-      : null;
-  const bottomCell =
-    rowParam < MAX_ROWS - 1 ? cells[rowParam + 1][colParam] : null;
-  const bottomRightCell =
-    rowParam < MAX_ROWS - 1 && colParam < MAX_COLS - 1
-      ? cells[rowParam + 1][colParam + 1]
-      : null;
+  const rightCell = colParam < MAX_COLS - 1 ? cells[rowParam][colParam + 1] : null;
+  const bottomLeftCell = rowParam < MAX_ROWS - 1 && colParam > 0 ? cells[rowParam + 1][colParam - 1] : null;
+  const bottomCell = rowParam < MAX_ROWS - 1 ? cells[rowParam + 1][colParam] : null;
+  const bottomRightCell = rowParam < MAX_ROWS - 1 && colParam < MAX_COLS - 1 ? cells[rowParam + 1][colParam + 1] : null;
 
   return {
     topLeftCell,
